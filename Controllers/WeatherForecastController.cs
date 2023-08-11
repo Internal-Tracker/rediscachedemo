4d8fde6659dc9b478ca008ee6b1c7e27605ba764
sushant yadav <sushant.yadav08@gmail.com>
2023-08-11 22:24:51 +0530

    update route path nin weather ctlr

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace rediscachedemoazure.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]/[action]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
